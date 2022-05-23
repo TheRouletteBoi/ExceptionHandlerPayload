5c73f7aee4a37dfa534c50def79e74e8bc0b77dd
TheRouletteBoi <therouletteboi@outlook.com>
2022-05-22 22:56:14 -0700

    Hook Interrupt Exception

--- src/Hooks.cpp
+++ src/Hooks.cpp
@@ -2,38 +2,69 @@
 #include "SpinLock.h"
 #include "PPC.h"
 #include "Libc.h"
-
 #include "LV2/LV2.h"
 #include "LV2/Process.h"
 #include "LV2/Memory.h"
 #include "LV2/Network.h"
 #include "LV2/Scheduler.h"
 #include "LV2/Thread.h"
-
-#include "Exploit.h"
-#include "UserMemory.h"
-
 #include "LV2/PRX.h"
-
+#include "UserMemory.h"
 #include "Hex.h"
 
 volatile int SystemCallLock;
 
 extern "C" void SystemCallHookPrepareDispatch();
-void PpuThreadMsgInterruptExceptionHook();
+//extern "C" void PpuThreadMsgInterruptExceptionHookPrepare();
+
+extern "C" void SystemCallHookProcess(SystemCallContext* callContext)
+{
+    // hook syscall load module and check path for hdd0/your_game then force load the sprx using this hook method :)
+}
+
+void PpuThreadMsgInterruptExceptionOriginal(...)
+{
+    __asm("nop");
+    __asm("nop");
+    __asm("nop");
+    __asm("nop");
+    __asm("nop");
+    __asm("nop");
+    __asm("nop");
+    __asm("nop");
+    __asm("nop");
+    __asm("nop");
+    __asm("nop");
+    __asm("nop");
+    __asm("nop");
+    __asm("nop");
+    __asm("nop");
+    __asm("nop");
+    __asm("nop");
+    __asm("nop");
+    __asm("nop");
+    __asm("nop");
+    __asm("nop");
+    __asm("nop");
+    __asm("nop");
+}
+
+void PpuThreadMsgInterruptExceptionHook(uint64_t thread_obj, uint64_t tb_value)
+{
+    DEBUG_PRINT("PpuThreadMsgInterruptExceptionHook\n");
+    DEBUG_PRINT("thread_obj: 0x%llX\n", thread_obj);
+    DEBUG_PRINT("tb_value: 0x%llX\n", tb_value);
+
+    //PpuThreadMsgInterruptExceptionOriginal(thread_obj, tb_value); // HookFunctionStart
+    lv2::ppu_thread_msg_interrupt_exception(thread_obj, tb_value);
+}
 
 void InstallHooks()
 {
     SpinLockInit(&SystemCallLock);
     PPCWriteBranchRelative(g_LibLV2.systemCallDispatchBranch, ((OPD_t*)SystemCallHookPrepareDispatch)->Function, true);
-}
 
-extern "C" void SystemCallHookProcess(SystemCallContext* callContext)
-{
-
-}
-
-void PpuThreadMsgInterruptExceptionHook()
-{
-    DEBUG_PRINT("PpuThreadMsgInterruptExceptionHook\n");
+    //PPCWriteBranchRelative((uint32_t*)g_LibLV2.ppu_thread_msg_interrupt_exception_opd->Function, ((OPD_t*)PpuThreadMsgInterruptExceptionHookPrepare)->Function, false);
+    //HookFunctionStart(g_LibLV2.ppu_thread_msg_interrupt_exception_opd->Function, ((OPD_t*)PpuThreadMsgInterruptExceptionHook)->Function, ((OPD_t*)PpuThreadMsgInterruptExceptionOriginal)->Function);
+    PPCWriteBranchRelative((uint32_t*)g_LibLV2.ppuThreadMsgInterruptExceptionBranch, ((OPD_t*)PpuThreadMsgInterruptExceptionHook)->Function, true);
 }
