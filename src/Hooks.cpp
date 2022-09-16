7e9bafe0434caf863e02396a5f6e1a9b55a861e9
TheRouletteBoi <therouletteboi@outlook.com>
2022-09-16 00:37:28 -0700

    Experiment with different hooking methods
    - Experiment with different hooking methods

--- src/Hooks.cpp
+++ src/Hooks.cpp
@@ -19,10 +19,36 @@ extern "C" void SystemCallHookPrepareDispatch();
 
 extern "C" void SystemCallHookProcess(SystemCallContext* callContext)
 {
-    // hook syscall load module and check path for hdd0/your_game then force load the sprx using this hook method :)
+    /*if (callContext->index == _SYS_PRX_LOAD_MODULE)
+    {
+        lv2::process* process  lv2::get_current_process();
+        if (process != nullptr)
+        {
+            if (process->ImageName != nullptr)
+            {
+                if (NonCryptoHashFNV1A64(process->ImageName) == NonCryptoHashFNV1A64("/app_home/PS3_GAME/USRDIR/EBOOT.BIN")
+                    || NonCryptoHashFNV1A64(process->ImageName) == NonCryptoHashFNV1A64("/dev_bdvd/PS3_GAME/USRDIR/EBOOT.BIN")
+                    || strstr(process->ImageName, "PS3_GAME/USRDIR/")
+                    || strstr(process->ImageName, "hdd0/game/"))
+                {
+                    STATIC_FN(&sys_prx_load_module, &callContext->handler)(callContext->args[0], callContext->args[1], callContext->args[2]);
+                }
+            }
+        }
+    }
+
+    if (callContext->index == SYS_DBG_WRITE_PROCESS_MEMORY)
+    {
+        process_write_memory(proc, callContext->args[1], callContext->args[3], callContext->args[2], 1);
+    }
+
+    if (callContext->index == SYS_DBG_READ_PROCESS_MEMORY)
+    {
+        // TODO...
+    }*/
 }
 
-void PpuThreadMsgInterruptExceptionOriginal(...)
+/*void PpuThreadMsgInterruptExceptionOriginal(...)
 {
     __asm("nop");
     __asm("nop");
@@ -49,7 +75,7 @@ void PpuThreadMsgInterruptExceptionOriginal(...)
     __asm("nop");
 }
 
-void PpuThreadMsgInterruptExceptionHook(uint64_t thread_obj, uint64_t tb_value)
+extern "C" void PpuThreadMsgInterruptExceptionHook(uint64_t thread_obj, uint64_t tb_value)
 {
     DEBUG_PRINT("PpuThreadMsgInterruptExceptionHook\n");
     DEBUG_PRINT("thread_obj: 0x%llX\n", thread_obj);
@@ -57,14 +83,28 @@ void PpuThreadMsgInterruptExceptionHook(uint64_t thread_obj, uint64_t tb_value)
 
     //PpuThreadMsgInterruptExceptionOriginal(thread_obj, tb_value); // HookFunctionStart
     lv2::ppu_thread_msg_interrupt_exception(thread_obj, tb_value);
-}
+}*/
+
+/*LV2_HOOKED_FUNCTION_PRECALL_SUCCESS_2(void, PpuThreadMsgInterruptExceptionHook, (uint64_t thread_obj, uint64_t tb_value))
+{
+    DEBUG_PRINT("PpuThreadMsgInterruptExceptionHook\n");
+    DEBUG_PRINT("thread_obj: 0x%llX\n", thread_obj);
+    DEBUG_PRINT("tb_value: 0x%llX\n", tb_value);
+
+
+
+}*/
 
 void InstallHooks()
 {
     SpinLockInit(&SystemCallLock);
     PPCWriteBranchRelative(g_LibLV2.systemCallDispatchBranch, ((OPD_t*)SystemCallHookPrepareDispatch)->Function, true);
 
+
+
     //PPCWriteBranchRelative((uint32_t*)g_LibLV2.ppu_thread_msg_interrupt_exception_opd->Function, ((OPD_t*)PpuThreadMsgInterruptExceptionHookPrepare)->Function, false);
-    //HookFunctionStart(g_LibLV2.ppu_thread_msg_interrupt_exception_opd->Function, ((OPD_t*)PpuThreadMsgInterruptExceptionHook)->Function, ((OPD_t*)PpuThreadMsgInterruptExceptionOriginal)->Function);
-    PPCWriteBranchRelative((uint32_t*)g_LibLV2.ppuThreadMsgInterruptExceptionBranch, ((OPD_t*)PpuThreadMsgInterruptExceptionHook)->Function, true);
+    //HookFunctionStart(g_LibLV2.ppu_thread_msg_interrupt_exception_opd->Function, ((OPD_t*)PpuThreadMsgInterruptExceptionHookPrepare)->Function, ((OPD_t*)PpuThreadMsgInterruptExceptionOriginal)->Function);
+    //PPCWriteBranchRelative((uint32_t*)g_LibLV2.ppuThreadMsgInterruptExceptionBranch, ((OPD_t*)PpuThreadMsgInterruptExceptionHookPrepare)->Function, true);
+    //hook_function_with_postcall(g_LibLV2.ppu_thread_msg_interrupt_exception_opd->Function, PpuThreadMsgInterruptExceptionHook, 2);
+    //hook_function_on_precall_success(g_LibLV2.ppu_thread_msg_interrupt_exception_opd->Function, (void*)((OPD_t*)PpuThreadMsgInterruptExceptionHook)->Function, 2);
 }
