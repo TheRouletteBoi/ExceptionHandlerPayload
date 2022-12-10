aef62ec01443d0bb09eababba1340db5743442e5
TheRouletteBoi <therouletteboi@outlook.com>
2022-12-10 08:10:58 -0800

    Add Make \bin PATH to bat file

--- build.bat
+++ build.bat
@@ -1,4 +1,10 @@
 @echo off
+
+set PATH=C:\Program Files\Microsoft Visual Studio\2022\Preview\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin\;%PATH% 
+
+if not exist %PATH%\cmake.exe set PATH=C:\Program Files\CMake\bin\;%PATH%
+
 cmake -Bbuild -GNinja -DCMAKE_MAKE_PROGRAM="%~dp0toolchain\bin\win-x86_64\ninja.exe"
 cmake --build build --clean-first
+
 pause
